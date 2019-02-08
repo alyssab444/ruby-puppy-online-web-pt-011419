ea424e056705409cb33512a4dea1893c8436dad1
Alyssa Brown <alyssa.brown@live.com>
2019-02-08 04:29:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,11 @@
 class Dog 
   @@all= []
+  @@clear_all 
   attr_accessor :name 
     def initialize(name)
       @name = name 
      @@all<< self 
+     @@clear_all 
     end  
     def self.all 
       @@all.each do |each_dog|
